efdfd03e0b628331a4bfa7e062e5158a2ad46ece
Warul K. Sinha <wksiniha@outlook.com>
2025-03-29 17:02:03 +0530

    Implement Prefix (KMP), Z-Function

--- pattern-matching/src/lib.rs
+++ pattern-matching/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// https://cp-algorithms.com/string/prefix-function.html#implementation
+pub fn prefix_function(s: &str) -> Vec<usize> {
+    let n = s.len();
+    let mut pi = vec![0; n];
+    let s = s.as_bytes();
+    
+    for i in 1..n {
+        let mut j = pi[i - 1];
+        while j > 0 && s[i] != s[j] {
+            j = pi[j - 1];
+        }
+        if s[i] == s[j] {
+            j += 1;
+        }
+        pi[i] = j;
+    }
+    
+    pi
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// https://cp-algorithms.com/string/z-function.html#implementation
+pub fn z_function(s: &str) -> Vec<usize> {
+    let n = s.len();
+    let mut z = vec![0; n];
+    let mut l = 0;
+    let mut r = 0;
+    let s = s.as_bytes();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    for i in 1..n {
+        if i < r {
+            z[i] = std::cmp::min(r - i, z[i - l]);
+        }
+        while i + z[i] < n && s[z[i]] == s[i + z[i]] {
+            z[i] += 1;
+        }
+        if i + z[i] > r {
+            l = i;
+            r = i + z[i];
+        }
     }
+    
+    z
 }
